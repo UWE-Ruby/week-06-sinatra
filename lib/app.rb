4e7014352ddf12056d87f473ced2026a484ee93b
Brandon Faloona <brandon@faloona.net>
2011-11-08 16:59:10 -0800

    Cleaned up app.rb

--- lib/app.rb
+++ lib/app.rb
@@ -7,25 +7,22 @@ class TwitterInfo < Sinatra::Application
   set :views, settings.root + '/../views'
 
   get '/' do
-    '<html><body>Append a twitter username on the url to display
-      how many followers the user has.<br/> For example:<br/><a href=\'/user/burtlo\'>/user/<strong>burtlo</strong></a>
-      </body></html>'
-  end
-
-  get /^\/user\/(\S+)$/ do |user|
-
-    @user = user
-
-    user_id = Twitter.user(@user).id
-
-    followers = Twitter.follower_ids(user_id).ids
-
-    @num_followers = followers.length
     haml :index
   end
 
+  get '/user/:username' do
+
+    @user = params[:username]
+    user_id = Twitter.user(@user).id
+    followers = Twitter.follower_ids(user_id).ids
+    @num_followers = followers.length
+
+    haml :followers
+
+  end
+
   post // do
-    [500, nil, 'Whoa. Sorry. No POSTs allowed.']
+    halt 500, 'Whoa. Sorry. No POSTs allowed.'
   end
 
 end
